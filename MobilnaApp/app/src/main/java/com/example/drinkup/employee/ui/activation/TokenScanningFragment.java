a8e2d183945030d69180044bd355906afc71a2db
Marinela Levak <marinela_levak@hotmail.com>
2020-12-28 18:30:16 +0100

    Učiǌeno da se ne vrši analiza slike kamere dok je prikazana Toast poruka
    na taj način se osigurava da nakon skeniranog Qr koda se ne izvrši odmah ponovna interpretacija istog Qr koda, već bi se to učinilo tek ukoliko konobar ostane snimati Qr kod do kraja isteka Toast poruke o uspjehu/pogrešci skeniranja

--- MobilnaApp/app/src/main/java/com/example/drinkup/employee/ui/activation/TokenScanningFragment.java
+++ MobilnaApp/app/src/main/java/com/example/drinkup/employee/ui/activation/TokenScanningFragment.java
@@ -40,6 +40,7 @@ public class TokenScanningFragment extends Fragment {
     private static final int PERMISSION_REQUEST_CAMERA = 0;
 
     private EmployeeMainActivity activity;
+    private Toast mToast;
     private boolean qrCodeProcessingInProgress = false;
 
     private PreviewView cameraView;
@@ -61,11 +62,7 @@ public class TokenScanningFragment extends Fragment {
         if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
             startCamera();
         } else {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.CAMERA)) {
-                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
-            } else {
-                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
-            }
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
         }
     }
 
@@ -112,93 +109,105 @@ public class TokenScanningFragment extends Fragment {
         imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(activity), new QRCodeImageAnalyzer(new QRCodeFoundListener() {
             @Override
             public void onQRCodeFound(String decodedQrCode) {
-                if (!qrCodeProcessingInProgress)
-                {
-                    qrCodeProcessingInProgress = true;
-                    String tokenId = decodedQrCode.substring(decodedQrCode.lastIndexOf('/') + 1);
-                    new RequestService(getContext()).retrieveOfferDetails(tokenId,
-                            new Consumer<JSONObject>() {
-                                @Override
-                                public void accept(JSONObject jsonObject) {
-                                    if (jsonObject == null) {
-                                        Toast.makeText(activity, R.string.token_no_longer_exists, Toast.LENGTH_LONG).show();
-                                        qrCodeProcessingInProgress = false;
-                                    }
-                                    else {
-                                        try {
-                                            boolean alreadyUsed = jsonObject.getBoolean("iskoristen");
-                                            String tokenId = jsonObject.getString("id");
-                                            if (alreadyUsed) {
-                                                Toast.makeText(getContext(), R.string.token_already_redeemed, Toast.LENGTH_LONG).show();
+                if (!qrCodeProcessingInProgress) {
+                    if (mToast == null || !mToast.getView().isShown()) {
+                        qrCodeProcessingInProgress = true;
+                        String tokenId = decodedQrCode.substring(decodedQrCode.lastIndexOf('/') + 1);
+                        new RequestService(getContext()).retrieveOfferDetails(tokenId,
+                                new Consumer<JSONObject>() {
+                                    @Override
+                                    public void accept(JSONObject jsonObject) {
+                                        if (jsonObject == null) {
+                                            showAToast(R.string.token_no_longer_exists);
+                                            qrCodeProcessingInProgress = false;
+                                        } else {
+                                            try {
+                                                boolean alreadyUsed = jsonObject.getBoolean("iskoristen");
+                                                String tokenId = jsonObject.getString("id");
+                                                if (alreadyUsed) {
+                                                    showAToast(R.string.token_already_redeemed);
+                                                    qrCodeProcessingInProgress = false;
+                                                } else {
+                                                    int barIdOfToken = jsonObject.getJSONObject("ponuda").getInt("objektId");
+                                                    String offerTitle = jsonObject.getJSONObject("ponuda").getString("naslov");
+                                                    String offerDescription = jsonObject.getJSONObject("ponuda").getString("opis");
+                                                    if (barIdOfToken != activity.getWorkingBarId()) {
+                                                        showAToast(R.string.token_not_applicable_here);
+                                                        qrCodeProcessingInProgress = false;
+                                                    } else {
+                                                        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                                                            @Override
+                                                            public void onClick(DialogInterface dialog, int option) {
+                                                                switch (option) {
+                                                                    case DialogInterface.BUTTON_POSITIVE:
+                                                                        new RequestService(getContext()).redeemToken(tokenId,
+                                                                                new Consumer<JSONObject>() {
+                                                                                    @Override
+                                                                                    public void accept(JSONObject jsonObject) {
+                                                                                        showAToast(R.string.token_successfully_activated);
+                                                                                        qrCodeProcessingInProgress = false;
+                                                                                    }
+                                                                                }, new Consumer<VolleyError>() {
+                                                                                    @Override
+                                                                                    public void accept(VolleyError volleyError) {
+                                                                                        showAToast(R.string.token_activation_failed);
+                                                                                        qrCodeProcessingInProgress = false;
+                                                                                    }
+                                                                                });
+                                                                        break;
+                                                                    case DialogInterface.BUTTON_NEGATIVE:
+                                                                        qrCodeProcessingInProgress = false;
+                                                                        break;
+
+                                                                }
+                                                            }
+                                                        };
+
+                                                        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                                                        builder.setMessage(MessageFormat.format(getString(R.string.redeem_token_confirmation_message), offerTitle, offerDescription))
+                                                                .setPositiveButton(R.string.redeem_option, dialogClickListener)
+                                                                .setNegativeButton(R.string.reject_option, dialogClickListener)
+                                                                .setOnCancelListener(new DialogInterface.OnCancelListener() {
+                                                                    @Override
+                                                                    public void onCancel(DialogInterface dialog) {
+                                                                        qrCodeProcessingInProgress = false;
+                                                                    }
+                                                                })
+                                                                .show();
+                                                    }
+                                                }
+                                            } catch (JSONException e) {
+                                                e.printStackTrace();
                                                 qrCodeProcessingInProgress = false;
                                             }
-                                            else {
-                                                int barIdOfToken = jsonObject.getJSONObject("ponuda").getInt("objektId");
-                                                String offerTitle = jsonObject.getJSONObject("ponuda").getString("naslov");
-                                                String offerDescription = jsonObject.getJSONObject("ponuda").getString("opis");
-                                                if (barIdOfToken != activity.getWorkingBarId()) {
-                                                    Toast.makeText(activity, R.string.token_not_applicable_here, Toast.LENGTH_LONG).show();
-                                                    qrCodeProcessingInProgress = false;
-                                                }
-                                                else {
-                                                    DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
-                                                        @Override
-                                                        public void onClick(DialogInterface dialog, int option) {
-                                                            switch (option) {
-                                                                case DialogInterface.BUTTON_POSITIVE:
-                                                                    new RequestService(getContext()).redeemToken(tokenId,
-                                                                            new Consumer<JSONObject>() {
-                                                                                @Override
-                                                                                public void accept(JSONObject jsonObject) {
-                                                                                    Toast.makeText(getContext(), R.string.token_successfully_activated, Toast.LENGTH_LONG).show();
-                                                                                    qrCodeProcessingInProgress = false;
-                                                                                }
-                                                                            }, new Consumer<VolleyError>() {
-                                                                                @Override
-                                                                                public void accept(VolleyError volleyError) {
-                                                                                    Toast.makeText(getContext(), R.string.token_activation_failed, Toast.LENGTH_LONG).show();
-                                                                                    qrCodeProcessingInProgress = false;
-                                                                                }
-                                                                            });
-                                                                    break;
-                                                                case DialogInterface.BUTTON_NEGATIVE:
-                                                                    qrCodeProcessingInProgress = false;
-                                                                    break;
-
-                                                            }
-                                                        }
-                                                    };
-
-                                                    AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-                                                    builder.setMessage(MessageFormat.format(getString(R.string.redeem_token_confirmation_message), offerTitle, offerDescription))
-                                                            .setPositiveButton(R.string.redeem_option, dialogClickListener)
-                                                            .setNegativeButton(R.string.reject_option, dialogClickListener)
-                                                            .setOnCancelListener(new DialogInterface.OnCancelListener() {
-                                                                @Override
-                                                                public void onCancel(DialogInterface dialog) {
-                                                                    qrCodeProcessingInProgress = false;
-                                                                }
-                                                            })
-                                                            .show();
-                                                }
-                                            }
-                                        } catch (JSONException e) {
-                                            e.printStackTrace();
-                                            qrCodeProcessingInProgress = false;
                                         }
                                     }
-                                }
-                            }, new Consumer<VolleyError>() {
-                                @Override
-                                public void accept(VolleyError volleyError) {
-                                    Toast.makeText(getContext(), R.string.token_info_retrieval_failed, Toast.LENGTH_LONG).show();
-                                    qrCodeProcessingInProgress = false;
-                                }
-                            });
+                                }, new Consumer<VolleyError>() {
+                                    @Override
+                                    public void accept(VolleyError volleyError) {
+                                        showAToast(R.string.token_info_retrieval_failed);
+                                        qrCodeProcessingInProgress = false;
+                                    }
+                                });
+                    } else {
+                        return;
+                    }
                 }
             }
         }));
 
         cameraProvider.bindToLifecycle(this, cameraSelector, imageAnalysis, preview);
+    }
+
+    // reference: https://stackoverflow.com/a/11441738/7519721
+    private void showAToast(int textCode) {
+        String st = getString(textCode);
+        try{
+            mToast.getView().isShown();     // true if visible
+            mToast.setText(st);
+        } catch (Exception e) {         // invisible if exception
+            mToast = Toast.makeText(getContext(), st, Toast.LENGTH_LONG);
+        }
+        mToast.show();  //finally display it
     }
 }
