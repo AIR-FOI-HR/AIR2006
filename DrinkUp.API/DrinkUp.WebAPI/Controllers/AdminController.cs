4922ab67fdf23c0e36065dc119907c6e9b336490
Kacko <karlo.kac.kk@gmail.com>
2021-01-21 14:54:59 +0100

    Unauthorized admin api

---
+++ DrinkUp.API/DrinkUp.WebAPI/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace DrinkUp.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class AdminController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Admin()
+        {
+            return Unauthorized();
+        }
+    }
+}
